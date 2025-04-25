8b982bba99dc02c9c40e9039223aa6a32b94163f
Md Rasel Khan <immdraselkhan@gmail.com>
2025-04-25 19:36:49 +0600

    first commit

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,6 +3,7 @@ export class Constants {
   static META_TITLE = "Next.js+shadcn Starter";
   static META_DESCRIPTION = "Next.js with TypeScript, Tailwind CSS, and shadcn";
   static NODE_ENV = process.env.NODE_ENV;
+  static PORT = process.env.PORT!;
   static BASE_URL = process.env.BASE_URL!;
   static BASE_API_URL = process.env.BASE_API_URL!;
 }
